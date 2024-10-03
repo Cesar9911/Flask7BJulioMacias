2334a86cf12c78a9d8367cbf08da8205d99a8dcb
Pichu <88215988+Cesar9911@users.noreply.github.com>
2024-10-02 22:43:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 import pusher
 import pytz
 
-# Configurar Pusher con las credenciales proporcionadas
 pusher_client = pusher.Pusher(
     app_id="1766032",
     key="e7b4efacf7381f83e05e",
@@ -13,7 +12,6 @@ pusher_client = pusher.Pusher(
 
 app = Flask(__name__)
 
-# Datos en memoria para reservas (en lugar de usar una base de datos)
 reservas = []
 id_counter = 1
 
@@ -21,7 +19,6 @@ id_counter = 1
 def index():
     return render_template("app.html")
 
-# Ruta para guardar los datos de las reservas enviadas desde el formulario
 @app.route("/reservar", methods=["POST"])
 def reservar():
     global id_counter
@@ -39,12 +36,10 @@ def reservar():
     id_counter += 1
     reservas.append(nueva_reserva)
 
-    # Enviar evento a través de Pusher
     pusher_client.trigger("canalReservaciones", "nueva-reserva", nueva_reserva)
 
     return jsonify(nueva_reserva), 200
 
-# Ruta para buscar las reservas (si es necesario)
 @app.route("/buscar", methods=["GET"])
 def buscar():
     return jsonify(reservas)
