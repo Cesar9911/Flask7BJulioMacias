20c5d3b8c8522f2bdd6c252d1f4b970b6f223793
Pichu <88215988+Cesar9911@users.noreply.github.com>
2024-10-02 20:05:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import pusher
 
 app = Flask(__name__)
 
-# Configura Pusher con las credenciales del profesor
+# Configura Pusher con tus credenciales
 pusher_client = pusher.Pusher(
-    app_id="1714541",
-    key="2df86616075904231311",
-    secret="2f91d936fd43d8e85a1a",
-    cluster="us2",
+    app_id='YOUR_APP_ID',
+    key='YOUR_KEY',
+    secret='YOUR_SECRET',
+    cluster='YOUR_CLUSTER',
     ssl=True
 )
 
